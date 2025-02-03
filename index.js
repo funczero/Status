24f5371009edaf50ec21e84a01ed58744ccdb8b1
FuncZero <contato@funczero.xyz>
2025-02-02 21:38:11 -0300

    Atualização Geral

--- index.js
+++ index.js
@@ -2,43 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const { Client, GatewayIntentBits } = require('discord.js');
-const os = require('os');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildPresences,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessages,
   ],
 });
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 80;
 
 app.use(express.json());
 app.use(require('cors')());
 
-const USER_ID = '1006909671908585586';
-const GUILD_ID = '1148661284594790400';
-const PREFIX = '.';
-
-function formatUptime(seconds) {
-  const months = Math.floor(seconds / (30 * 24 * 60 * 60));
-  seconds %= 30 * 24 * 60 * 60;
-
-  const days = Math.floor(seconds / (24 * 60 * 60));
-  seconds %= 24 * 60 * 60;
-
-  const hours = Math.floor(seconds / (60 * 60));
-  seconds %= 60 * 60;
-
-  const minutes = Math.floor(seconds / 60);
-  seconds = Math.floor(seconds % 60);
-
-  return `${months} mese(s), ${days} dia(s), ${hours} hora(s), ${minutes} minuto(s) e ${seconds} segundos`;
-}
+const USER_ID = process.env.USER_ID;
+const GUILD_ID = process.env.GUILD_ID;
 
 app.get('/status', async (req, res) => {
   try {
@@ -76,45 +56,6 @@ client.once('ready', () => {
   });
 
   console.log('Status do bot configurado para "Assistindo: https://funczero.xyz".');
-});
-
-client.on('messageCreate', async (message) => {
-  if (message.author.bot || !message.content.startsWith(PREFIX)) return;
-
-  const args = message.content.slice(PREFIX.length).trim().split(/ +/);
-  const command = args.shift().toLowerCase();
-
-  if (command === 'ping') {
-    const msg = await message.channel.send('Calculando...');
-
-    const latency = msg.createdTimestamp - message.createdTimestamp;
-    const apiPing = client.ws.ping;
-
-    const cpuUsage = process.cpuUsage();
-    const memoryUsage = process.memoryUsage();
-
-    const cpuPercentage = ((cpuUsage.user + cpuUsage.system) / 1e6).toFixed(2);
-    const memoryMB = (memoryUsage.heapUsed / 1024 / 1024).toFixed(2);
-    const totalMemoryMB = (os.totalmem() / 1024 / 1024).toFixed(2);
-
-    const uptime = formatUptime(process.uptime());
-
-    const embed = {
-      color: 0x00ff00,
-      title: 'Informações do Sistema',
-      fields: [
-        { name: 'Ping da API', value: `${apiPing}ms`, inline: true },
-        { name: 'Latência', value: `${latency}ms`, inline: true },
-        { name: 'Uso de CPU', value: `${cpuPercentage}%`, inline: true },
-        { name: 'Uso de Memória', value: `${memoryMB}MB / ${totalMemoryMB}MB`, inline: true },
-        { name: 'Sistema Operacional', value: `${os.type()} ${os.release()}`, inline: true },
-        { name: 'Uptime do Bot', value: uptime, inline: true },
-      ],
-      timestamp: new Date(),
-    };
-
-    msg.edit({ content: null, embeds: [embed] });
-  }
 });
 
 client.login(process.env.TOKEN);
